6f49cefc76c7c63a3ddeb80cccfe49780cabd709
kleu046 <kalokleung@Kas-iMac.local>
2021-10-29 00:28:51 +1300

    updated help file content

--- R/wr_renameCols.R
+++ R/wr_renameCols.R
@@ -2,6 +2,11 @@
 #' @description using data.table packages setnames function to change column
 #'   names
 #' @usage wr_renameCols(dt, old=NULL, new=NULL, ...)
+#' @param dt input data.table
+#' @param old default = NULL vector of existing column names
+#' @param new default = NULL vector of new column names
+#' @param ... non-standard evaluation of in the form of an expression such as,
+#'   old_column_name = new_column_name
 #' @details User can use provide a vector of characters for current column names
 #'   and a vector of characters of equal lengths as new column names.
 #'   Alternatively, user can also use the old_column_name=new_column_name
