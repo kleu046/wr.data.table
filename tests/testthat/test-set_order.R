d2c5384a2bee2861d08c620f78b6041c0c4ac479
kleu046 <kalokleung@Kas-iMac.local>
2021-10-31 00:09:21 +1300

    setorder and setkey are exactly the same as Rdata.table and they both do produce an output for piping

--- tests/testthat/test-set_order.R
+++
@@ -1,9 +0,0 @@
-mkdt()
-dt_ordered <- set_order(dt, vs, am)
-
-test_that("data.table is ordered", {
-  expect_s3_class(dt_ordered, "data.table")
-  expect_identical(dt_ordered, dt[order(vs, am),])
-})
-
-rm(dt)
