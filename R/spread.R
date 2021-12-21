ec0ff7ba8906e49a11cd02ebd62aa29424f95102
kleu046 <rkleu046@gmail.com>
2021-12-21 23:15:24 +1300

    added dcast example for future work in .R file

--- R/spread.R
+++ R/spread.R
@@ -1,4 +1,4 @@
-#' long to short data
+#' Long to short data
 #' @usage spread(dt, key, value)
 #' @param dt input data.table
 #' @param key key variable used to spread data
@@ -8,9 +8,12 @@
 spread <- function(dt, key, value) {
   key <- paste0("`",deparse(substitute(key)),"`")
   value <- deparse(substitute(value))
+
   SDcols <- paste0("`",colnames(dt),"`")
   SDcols <- SDcols[c(-grep(key, SDcols),-grep(paste0("`",value,"`"), SDcols))]
-  SDcols <- paste(paste(SDcols, collapse="+"))
+  SDcols <- paste(SDcols, collapse="+")
   eval(parse(text=paste0("dcast(dt,",SDcols,"~",key,",value.var=\"",value,"\")")))
 }
 
+# dcast example
+# dcast(dt, cyl + disp + hp + drat + wt + qsec + vs + am + carb ~ gear, value.var = "mpg")
