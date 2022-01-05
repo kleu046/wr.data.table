b59b1e6b24972d164faaf55a230bb20b4b5eff1a
kleu046 <kalokleung@Kas-iMac.local>
2022-01-05 21:09:14 +1300

    def_cols improvement

--- R/def_cols.R
+++ R/def_cols.R
@@ -1,40 +1,40 @@
 #' Create and assign new columns in data.table
-#' @description
-#'
-#' For more information about \code{data.table} and \code{setorder()} \href{http://www.google.com}{Google it}.
+#' @description function used to create new columns in data.table using non-standard evaluation
 #'
 #' @usage def_cols(dt, ...)
 #'
 #' @param dt a \code{data.table}
-#' @param ... expression setting up a new column.  E.g. kpl = mpg / 2.35215.
-#' More than one new columns can be created by having expressions separated by commas
+#' @param ... formula using `~` notation where LHS is the new column name and RHS is an expression for the values
+#' More than one new columns can be created by having formulas separated by commas
 #'
 #' @return returns a \code{data.table}
 #'
 #' @importFrom data.table copy
+#' @importFrom furrr future_map
+#' @importFrom rlang caller_env
 #'
 #' @examples
 #' \dontrun{
-  #' dt <- data.table(mtcars)
-  #' def_cols(dt, kmpl = mpg / 2.35215, wt_in_kg = wt / 2.20462)
-  #' # creating two columns / converted units
-  #' def_cols(dt, am = NULL) # remove a column
+#' dt <- data.table(mtcars)
+#' define(dt, kmpl ~ mpg / 2.35215, wt_in_kg ~ wt / 2.20462)
+#' # creating two columns / converted units
+#' define(dt, am = NULL) # remove a column
 #' }
 #' @export
 def_cols <- function(dt, ...) {
-  # dots = named list of ...
-  # names = names for new columns
-  # values in list = values for assignment
-
+  env <- caller_env()
   dots <- substitute(list(...))
 
-  dots <- paste(
-    paste(paste0("`",names(dots[-1]),"`"),
-          paste0(dots[-1]),
-          sep="="),
-    collapse=",")
+  nm <- future_map(2:length(dots), function(i) paste0(dots[[i]][[2]]))
 
-  callAsString <- paste0("data.table::copy(dt)[,`:=`(",dots,")]")
+  data <-
+    future_map(2:length(dots), function(i) {
+      eval(dots[[i]][[3]], envir=dt, enclos=env)
+    })
 
-  eval(parse(text=callAsString))
+  copy_dt <- copy(dt)
+  for (i in 1:length(nm)) {
+    copy_dt[,nm[[i]]:=data[[i]]]
+  }
+  copy_dt
 }
